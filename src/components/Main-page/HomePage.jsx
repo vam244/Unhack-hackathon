5aeb5b89d854e8189ffd065576a9073e38382f47
Nitin <nk17oct@gmail.com>
2023-03-17 19:59:01 +0530

    fix devfolio button

--- src/components/Main-page/HomePage.jsx
+++ src/components/Main-page/HomePage.jsx
@@ -127,16 +127,16 @@ export default function HomePage(props) {
 
 
 
-  React.useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://apply.devfolio.co/v2/sdk.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  // React.useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "https://apply.devfolio.co/v2/sdk.js";
+  //   script.async = true;
+  //   script.defer = true;
+  //   document.body.appendChild(script);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
   return (
     <div id="top" className="Whole_div">
