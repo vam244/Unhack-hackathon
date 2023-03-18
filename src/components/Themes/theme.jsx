a60e158db3e740afa239768615dc3ec0b7084c24
Nitin <nk17oct@gmail.com>
2023-03-18 11:41:46 +0530

    theme img

--- src/components/Themes/theme.jsx
+++ src/components/Themes/theme.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './theme.css';
+import education from './img/education.jpg';
+import health from './img/health.jpeg';
+import judicial from './img/judicial_system.jpg';
+import agri from './img/agriculture.jpg';
+import transportation from './img/transportation.jpg';
 
 
 function ThemeType(props) {
@@ -12,7 +17,7 @@ function ThemeType(props) {
                 <div class="cards_wrap">
                     <div class="card_item">
                         <div class="card_inner">
-                            <img src="./images/env1.jpg" /><br/><br/>
+                            <img src={education}/><br/><br/>
                             <div class="role_name">Education</div>
                             
                             {/*<div class="film">The list of issues surrounding our environment go on.These environmental problems arise from conflicts between nature and humans. Develop a Solution to reduce Envirnmental problem.</div>*/}
@@ -20,7 +25,7 @@ function ThemeType(props) {
                     </div>
                     <div class="card_item">
                         <div class="card_inner">
-                            <img src="./images/open_inno1.png" /><br/><br/>
+                            <img src={health} /><br/><br/>
                             <div class="role_name">Health and Wellness</div>
                             
                             {/*<div class="film">Have any ideas that don't fit into any of the categories above? Worry not. We encourage you to test your ideas and discover solutions to problems without any limitations.</div>*/}
@@ -28,7 +33,7 @@ function ThemeType(props) {
                     </div>
                     <div class="card_item">
                         <div class="card_inner">
-                            <img src="./images/web31.jpg" /><br/><br/>
+                            <img src={judicial} /><br/><br/>
                             <div class="role_name">Judicial System</div>
                             
                            {/* <div class="film">Web 3.0 is the third generation of internet services for websites and apps that will focus on providing a data-driven and Semantic Web employing a machine-based understanding of data.</div>*/}
@@ -37,7 +42,7 @@ function ThemeType(props) {
             
                     <div class="card_item">
                         <div class="card_inner">
-                            <img src="./images/edu.png" /><br/><br/>
+                            <img src={agri} /><br/><br/>
                             <div class="role_name">Agriculture</div>
                             
                            {/* <div class="film">Since the pandemic, the educational system has had to adjust to the new normal. As a result, we urge you to create something that modernises education and makes it accessible to everybody.</div>*/}
@@ -45,7 +50,7 @@ function ThemeType(props) {
                     </div>
                     <div class="card_item">
                         <div class="card_inner">
-                            <img src= "./images/health_care1.jpg"/><br/><br/>
+                            <img src= {transportation}/><br/><br/>
                             <div class="role_name">Transportation</div>
                             
                             {/*<div class="film">Develop Solution to help Society Cope with the Drastic impact. The Healthcare track strives to develop splitting initiatives that address a variety of health-related issues.</div>*/}
